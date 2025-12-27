1462cd20db1bbbbb17111cdd83308a736ef795d0
Lucas Couttulenc <lucascouttu@gmail.com>
2025-12-27 02:50:57 -0300

    Websocket terminado
    Hice todo lo de websocket para la conexión con el cliente (la estructura, faltarían obviamente funciones de tipo endpoint para manejar los distintos tipos de mensajes).
    Lo siguiente que quiero hacer es hacer en JS la parte del socket para chequear que ande.

--- lib/echo/chats/chat.ex
+++ lib/echo/chats/chat.ex
@@ -1,5 +1,21 @@
 defmodule Echo.Chats.Chat do
-  # Es el que define la estructura y logica del chat
+    # Define la estructura del usuario con el schema
+  # Llama a la DB para querys correspondientes a Usuarios
+
+
+  # Funciones que (probablemente) usen Ecto...
+
+  def get(id) do
+
+  end
+
+  def get_by_algo(username) do
+
+  end
+
+  def update_smth(smth) do
+
+  end
 
 
 
