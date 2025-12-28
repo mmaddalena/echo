40392ce72d61cdaceb6a5ec57bda552d3eca98ff
Lucas Couttulenc <lucascouttu@gmail.com>
2025-12-28 19:37:58 -0300

    Funcionalidad User + Envío de info en el login
    Pusheo porque estoy por mover el repo local a otra carpeta, asi no hay problemas :)

--- lib/echo/users/user.ex
+++ lib/echo/users/user.ex
@@ -1,20 +1,33 @@
 defmodule Echo.Users.User do
-  # Define la estructura del usuario con el schema
   # Llama a la DB para querys correspondientes a Usuarios
-
 
   # Funciones que (probablemente) usen Ecto...
 
-  def get(id) do
+  alias Echo.Repo
+  alias Echo.Schemas.User
 
+  def get(id) do
+    Repo.get(User, id)
   end
 
   def get_by_username(username) do
-
+    Repo.get_by(User, username: username)
   end
 
-  def update_smth(smth) do
+  def update_username(user_id, new_username) do
+    user = Repo.get(User, user_id)
 
+    case user do
+      nil ->
+        {:error, :not_found}
+
+      user ->
+        user
+        |> User.changeset(%{username: new_username})
+        |> Repo.update()
+        # Devuelve {:ok, %User{}} o {:error, %Ecto.Changeset{}}
+    end
   end
+
 
 end
