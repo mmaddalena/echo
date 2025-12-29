3b22b47bc76f08ce24c64f23a7fcb4eef0a7453e
Lucas Couttulenc <lucascouttu@gmail.com>
2025-12-28 23:34:42 -0300

    Front Creado
    Añadí el esqueleto inicial del front, y agregué al README (del front) cómo correrlo.

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <h1>Echo Front</h1>
+    <p>Conectando con el backend...</p>
+    <a href="https://github.com/rochimgg/echo" target="_blank">
+      <img src="/Echo_Logo_Completo_Negativo.svg" class="logo" alt="Echo logo" />
+    </a>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+</style>
