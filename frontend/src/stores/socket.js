3a9ba021175d2d9bb5f99a640d3ced7845194542
Martin Andres Maddalena <mmaddalena@fi.uba.ar>
2026-01-15 11:43:16 -0300

    config for observer added

--- frontend/src/stores/socket.js
+++ frontend/src/stores/socket.js
@@ -2,71 +2,69 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useSocketStore = defineStore("socket", () => {
-  const socket = ref(null);
-  const userInfo = ref(null);
-  const chats = ref([]);
-  const chatInfo = ref(null);
+	const socket = ref(null);
+	const userInfo = ref(null);
+	const chats = ref([]);
+	const chatInfo = ref(null);
 
-  function connect(token) {
-    if (socket.value) return; // ya conectado
+	function connect(token) {
+		if (socket.value) return; // ya conectado
 
-    socket.value = new WebSocket(`ws://localhost:4000/ws?token=${token}`);
+		socket.value = new WebSocket(`ws://localhost:4000/ws?token=${token}`);
 
-    socket.value.onopen = () => {
-      console.log("WS conectado");
-    };
+		socket.value.onopen = () => {
+			console.log("WS conectado");
+		};
 
-    socket.value.onmessage = (event) => {
-      const payload = JSON.parse(event.data);
+		socket.value.onmessage = (event) => {
+			const payload = JSON.parse(event.data);
 
-      if (payload.type === "user_info") {
-        userInfo.value = payload.user;
-        chats.value = payload.last_chats ?? [];
-      }
-      else if (payload.type === "chat_info") {
-        chatInfo.value = payload.chat;
-      } else if (payload.type === "new_message") {
-        
-      }
-    };
+			if (payload.type === "user_info") {
+				userInfo.value = payload.user;
+				chats.value = payload.last_chats ?? [];
+			} else if (payload.type === "chat_info") {
+				chatInfo.value = payload.chat;
+			} else if (payload.type === "new_message") {
+			}
+		};
 
-    socket.value.onerror = () => {
-      console.error("Error en WS");
-    };
-  }
+		socket.value.onerror = () => {
+			console.error("Error en WS");
+		};
+	}
 
-  function disconnect() {
-    if (socket.value) {
-      socket.value.close();
-    }
-    socket.value = null;
-    userInfo.value = null;
-    chatInfo.value = null;
-  }
+	function disconnect() {
+		if (socket.value) {
+			socket.value.close();
+		}
+		socket.value = null;
+		userInfo.value = null;
+		chatInfo.value = null;
+	}
 
-  function send(data) {
-    if (socket.value?.readyState === WebSocket.OPEN) {
-      socket.value.send(JSON.stringify(data));
-    }else{
-      console.error("El socket no está abierto")
-    }
-  }
+	function send(data) {
+		if (socket.value?.readyState === WebSocket.OPEN) {
+			socket.value.send(JSON.stringify(data));
+		} else {
+			console.error("El socket no está abierto");
+		}
+	}
 
-  function openChat(chatId) {
-    send({
-      type: "open_chat",
-      chat_id: chatId
-    })
-  }
+	function openChat(chatId) {
+		send({
+			type: "open_chat",
+			chat_id: chatId,
+		});
+	}
 
-  return {
-    socket,
-    userInfo,
-    chats,
-    chatInfo,
-    connect,
-    disconnect,
-    send,
-    openChat
-  };
+	return {
+		socket,
+		userInfo,
+		chats,
+		chatInfo,
+		connect,
+		disconnect,
+		send,
+		openChat,
+	};
 });
