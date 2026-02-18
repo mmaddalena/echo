9fb5bd8f6a771627a2d0125e7f2e860c66a74f34
mmaddalena <mmaddalena@fi.uba.ar>
2026-02-18 01:04:35 -0300

    feat: computed added for RegisterView

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import { useRouter } from "vue-router";
 import { useSocketStore } from "@/stores/socket";
 import { useThemeStore } from "@/stores/theme";
